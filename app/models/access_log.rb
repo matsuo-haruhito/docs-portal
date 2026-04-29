6bb8d49758eeee5944d7f031ddbf946863d59938
matsuo-haruhito <matsuo@barnet.co.jp>
2026-04-30 01:25:16 +0900

    public_id対応

--- app/models/access_log.rb
+++ app/models/access_log.rb
@@ -7,5 +7,14 @@ class AccessLog < ApplicationRecord
 
   enum :action_type, { view: 0, download: 1 }
 
+  before_validation :ensure_public_id, on: :create
+
+  validates :public_id, presence: true, uniqueness: true
   validates :target_type, :accessed_at, presence: true
+
+  private
+
+  def ensure_public_id
+    self.public_id ||= "alog_#{SecureRandom.urlsafe_base64(16)}"
+  end
 end
