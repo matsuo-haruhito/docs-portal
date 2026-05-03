9165b2cc41ff43c7038699db2b3b4df0b8638c89
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-04 04:46:19 +0900

    Add document version archive controller

---
+++ app/controllers/document_version_archives_controller.rb
@@ -0,0 +1,17 @@
+class DocumentVersionArchivesController < BaseController
+  def show
+    version = DocumentVersion.find_by!(public_id: params[:document_version_public_id])
+    require_document_version_view_access!(version)
+
+    archive = DocumentVersionZipBuilder.new(version:, user: current_user)
+    filename = archive.filename
+    record_zip_download_access_log(version, filename)
+
+    send_data(
+      archive.to_binary,
+      filename:,
+      type: "application/zip",
+      disposition: "attachment"
+    )
+  end
+end
