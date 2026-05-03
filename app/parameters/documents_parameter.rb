c2e6860e1969ed09fccb1860b509dfe71cb28401
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-04 06:55:20 +0900

    Expand rparam document filter parameters

--- app/parameters/documents_parameter.rb
+++ app/parameters/documents_parameter.rb
@@ -1,7 +1,18 @@
 class DocumentsParameter < Rparam::Parameter
+  INDEX_FILTERS = %i[q tag category document_kind visibility_policy has_html has_files has_pdf has_diagram page].freeze
+  BOOLEAN_FILTERS = %i[has_html has_files has_pdf has_diagram].freeze
+
   def index
     param :q, type: String
     param :tag, type: String
+    param :category, type: String, inclusion: Document.categories.keys
+    param :document_kind, type: String, inclusion: Document.document_kinds.keys
+    param :visibility_policy, type: String, inclusion: Document.visibility_policies.keys
+
+    BOOLEAN_FILTERS.each do |name|
+      param name, type: :boolean
+    end
+
     param :page, type: Integer, min: 1, default: 1
   end
 end
