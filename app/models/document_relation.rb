f09826c8eb61e1fa732aff897303412afc6c2284
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-03 22:39:08 +0900

    Add document relation model

---
+++ app/models/document_relation.rb
@@ -0,0 +1,30 @@
+class DocumentRelation < ApplicationRecord
+  include PublicIdentifiable
+
+  public_id_prefix "drel"
+
+  belongs_to :source_document, class_name: "Document"
+  belongs_to :target_document, class_name: "Document"
+
+  enum :relation_type, {
+    related: 0,
+    prerequisite: 1,
+    appendix: 2,
+    source: 3,
+    output: 4,
+    previous_version: 5
+  }
+
+  validates :source_document, :target_document, :relation_type, presence: true
+  validates :target_document_id, uniqueness: { scope: %i[source_document_id relation_type] }
+  validate :different_documents
+
+  private
+
+  def different_documents
+    return if source_document_id.blank? || target_document_id.blank?
+    return if source_document_id != target_document_id
+
+    errors.add(:target_document, "must be different from source document")
+  end
+end
