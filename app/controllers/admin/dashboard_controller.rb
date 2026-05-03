734b9465f98bd2b4f6cec61235628ac9dd6228d3
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-04 05:00:39 +0900

    Show document file health on admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,6 @@ class Admin::DashboardController < Admin::BaseController
   before_action :require_admin_only!
 
   def index
+    @document_file_health = DocumentFileHealthCheck.new.call
   end
 end
