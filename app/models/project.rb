ec714b914403c99465b25a6bac3bdacff575043a
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-05 02:43:22 +0900

    Associate projects with document sets

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,6 +5,7 @@ class Project < ApplicationRecord
   public_id_prefix "prj"
 
   has_many :documents, dependent: :destroy
+  has_many :document_sets, dependent: :destroy
   has_many :project_memberships, dependent: :destroy
   has_many :users, through: :project_memberships
 
