68e420ab03ecbb256dd443d0d562fd1064cb8acd
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-05 02:04:41 +0900

    Add document delivery log builder

---
+++ app/services/document_delivery_log_builder.rb
@@ -0,0 +1,43 @@
+class DocumentDeliveryLogBuilder
+  def initialize(sender:, project:, document: nil, attributes: {})
+    @sender = sender
+    @project = project
+    @document = document
+    @attributes = attributes
+  end
+
+  def build
+    validate_sender!
+    validate_document!
+
+    DocumentDeliveryLog.new(default_attributes.merge(attributes))
+  end
+
+  def create!
+    build.tap(&:save!)
+  end
+
+  private
+
+  attr_reader :sender, :project, :document, :attributes
+
+  def validate_sender!
+    raise ActiveRecord::RecordNotFound, "Project not found" unless project.accessible_to?(sender)
+  end
+
+  def validate_document!
+    return if document.blank?
+    raise ActiveRecord::RecordNotFound, "Document not found" unless document.project == project
+    raise ActiveRecord::RecordNotFound, "Document not found" unless document.viewable_by?(sender)
+  end
+
+  def default_attributes
+    {
+      project:,
+      document:,
+      sender:,
+      delivery_type: :portal_link,
+      status: :draft
+    }
+  end
+end
