b0e9083b6257d6403e398cfe185dc63d635cc33b
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-04 21:18:38 +0900

    Create document tag directly in tagging spec

--- spec/models/document_tagging_spec.rb
+++ spec/models/document_tagging_spec.rb
@@ -2,9 +2,11 @@ require "rails_helper"
 
 RSpec.describe DocumentTagging, type: :model do
   it "does not allow a negative sort order" do
+    tag = DocumentTag.create!(name: "重要", normalized_name: "重要")
+
     tagging = described_class.new(
       document: create(:document),
-      document_tag: create(:document_tag),
+      document_tag: tag,
       sort_order: -1
     )
 
