f2d39f4f219228599af24b9a589cdf92d0bde18b
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-05 06:58:55 +0900

    Add document tree refresh broadcaster

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,9 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  private
+
+  def broadcast_document_tree_refresh_later
+    broadcast_refresh_later_to "document_tree"
+  end
 end
