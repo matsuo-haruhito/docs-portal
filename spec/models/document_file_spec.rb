89f2debf3a961e71486d9f059fcd7a8c00eb4700
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-05 00:07:35 +0900

    Add document file scan status specs

--- spec/models/document_file_spec.rb
+++ spec/models/document_file_spec.rb
@@ -35,4 +35,57 @@ RSpec.describe DocumentFile, type: :model do
     expect(file).not_to be_valid
     expect(file.errors[:sort_order]).to be_present
   end
+
+  describe "scan status delivery gate" do
+    let(:external_user) { build(:user, :external) }
+    let(:internal_user) { build(:user, :internal) }
+
+    it "blocks external delivery while scan is pending but allows internal operational access" do
+      file = build_file(scan_status: :scan_pending)
+
+      expect(file).to be_blocked_by_scan
+      expect(file.deliverable_after_scan?(external_user)).to be(false)
+      expect(file.deliverable_after_scan?(internal_user)).to be(true)
+    end
+
+    it "allows external delivery after the scan is clean" do
+      file = build_file(scan_status: :scan_clean)
+
+      expect(file).not_to be_blocked_by_scan
+      expect(file.deliverable_after_scan?(external_user)).to be(true)
+    end
+
+    it "blocks external delivery when the scan detects danger" do
+      file = build_file(scan_status: :scan_infected)
+
+      expect(file).to be_blocked_by_scan
+      expect(file.deliverable_after_scan?(external_user)).to be(false)
+    end
+  end
+
+  describe "downloadable_by?" do
+    let(:company) { create(:company) }
+    let(:project) { create(:project) }
+    let(:document) { create(:document, project:, visibility_policy: :restricted) }
+    let(:version) { create(:document_version, document:, status: :published) }
+    let(:external_user) { create(:user, :external, company:) }
+    let(:internal_user) { create(:user, :internal) }
+
+    it "allows internal users regardless of scan status" do
+      file = create(:document_file, document_version: version, scan_status: :scan_pending)
+
+      expect(file.downloadable_by?(internal_user)).to be(true)
+    end
+
+    it "blocks external users until the file is clean" do
+      create(:document_permission, document:, company:, access_level: :download)
+      file = create(:document_file, document_version: version, scan_status: :scan_pending)
+
+      expect(file.downloadable_by?(external_user)).to be(false)
+
+      file.scan_clean!
+
+      expect(file.downloadable_by?(external_user)).to be(true)
+    end
+  end
 end
