2cc7ffce7296951600f9d136e4d460b02c531529
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-04 23:51:52 +0900

    Show configuration diagnostics on admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,7 @@ class Admin::DashboardController < Admin::BaseController
   before_action :require_admin_only!
 
   def index
+    @configuration_diagnostic = ApplicationConfigurationDiagnostic.new.call
     @document_file_health = DocumentFileHealthCheck.new.call
   end
 end
