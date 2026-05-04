34e494ee519ddbcac881cfbad60ee15c25ff6ba2
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-04 22:02:42 +0900

    Add source metadata indexes to document versions

---
+++ db/migrate/20260504131000_add_source_metadata_indexes_to_document_versions.rb
@@ -0,0 +1,11 @@
+class AddSourceMetadataIndexesToDocumentVersions < ActiveRecord::Migration[8.1]
+  def change
+    add_index :document_versions,
+              :source_basename,
+              name: "index_document_versions_on_source_basename"
+
+    add_index :document_versions,
+              :source_directory,
+              name: "index_document_versions_on_source_directory"
+  end
+end
