a0d120633e8e38fc9593c936136826f4815ed287
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-07 03:13:18 +0900

    Add project consent setting factory

--- spec/factories/consents.rb
+++ spec/factories/consents.rb
@@ -12,5 +12,13 @@ FactoryBot.define do
     association :user
     association :consent_term
     consented_at { Time.current }
+    consent_term_version_label { consent_term.version_label }
+  end
+
+  factory :project_consent_setting do
+    association :project
+    association :consent_term
+    required_on { :first_access }
+    enabled { true }
   end
 end
