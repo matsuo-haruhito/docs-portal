a241c8ffb7f93c9e913597ab8db5ae3df6110001
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-07 00:08:36 +0900

    Filter project page documents by visible latest version

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,8 +6,7 @@ class ProjectsController < BaseController
   def show
     @project = Project.find_by!(code: params[:code])
     require_project_access!(@project)
-    @documents = @project.documents.accessible_to(current_user).includes(:latest_version).recommended_first
-    @documents = @documents.select { _1.visible_in_portal_for?(current_user) } unless current_user.internal?
+    @documents = portal_documents_for(@project)
     @important_documents = @documents.select { %w[critical important].include?(_1.importance_level) }
     @tree_projects = portal_tree_projects(include_project: @project)
   end
@@ -20,14 +19,23 @@ class ProjectsController < BaseController
       .order(:code)
     return projects if current_user.internal?
 
-    visible_projects = projects.select { visible_project_for_portal?(_1) }
+    visible_projects = projects.select { portal_documents_for(_1).any? || _1.documents.empty? }
     visible_projects << include_project if include_project.present? && visible_projects.exclude?(include_project)
     visible_projects
   end
 
-  def visible_project_for_portal?(project)
-    return true if project.documents.empty?
+  def portal_documents_for(project)
+    documents = project.documents.accessible_to(current_user).includes(:latest_version).recommended_first
+    return documents if current_user.internal?
 
-    project.documents.any? { _1.visible_in_portal_for?(current_user) }
+    documents.select { externally_portal_visible_document?(_1) }
+  end
+
+  def externally_portal_visible_document?(document)
+    return false unless document.visible_in_portal_for?(current_user)
+    return true unless document.document_versions.exists?
+
+    latest_version = document.latest_version
+    latest_version.present? && latest_version.viewable_by?(current_user)
   end
 end
