f3783163c32785a82d40cc2063045bcf4e9967e5
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-07 02:51:55 +0900

    Add admin project consent settings controller

---
+++ app/controllers/admin/project_consent_settings_controller.rb
@@ -0,0 +1,54 @@
+class Admin::ProjectConsentSettingsController < Admin::BaseController
+  before_action :require_admin_only!
+  before_action :set_project_consent_setting, only: %i[edit update destroy]
+  before_action :load_form_collections, only: %i[index create edit update]
+
+  def index
+    @project_consent_settings = ProjectConsentSetting.includes(:project, :consent_term).order("projects.code", :required_on)
+    @project_consent_setting = ProjectConsentSetting.new(enabled: true, required_on: :first_access)
+  end
+
+  def create
+    @project_consent_setting = ProjectConsentSetting.new(project_consent_setting_params)
+
+    if @project_consent_setting.save
+      redirect_to admin_project_consent_settings_path, notice: "案件同意設定を登録しました。"
+    else
+      @project_consent_settings = ProjectConsentSetting.includes(:project, :consent_term).order("projects.code", :required_on)
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @project_consent_setting.update(project_consent_setting_params)
+      redirect_to admin_project_consent_settings_path, notice: "案件同意設定を更新しました。"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @project_consent_setting.destroy!
+    redirect_to admin_project_consent_settings_path, notice: "案件同意設定を削除しました。"
+  rescue ActiveRecord::DeleteRestrictionError, ActiveRecord::InvalidForeignKey
+    redirect_to admin_project_consent_settings_path, alert: "関連データがあるため削除できません。"
+  end
+
+  private
+
+  def set_project_consent_setting
+    @project_consent_setting = ProjectConsentSetting.find_by!(public_id: params[:id])
+  end
+
+  def load_form_collections
+    @projects = Project.order(:code)
+    @consent_terms = ConsentTerm.active_only.order(:title, :version_label)
+  end
+
+  def project_consent_setting_params
+    params.require(:project_consent_setting).permit(:project_id, :consent_term_id, :required_on, :enabled)
+  end
+end
