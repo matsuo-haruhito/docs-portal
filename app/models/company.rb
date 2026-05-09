057bc7a5665f62119527eb2f69951763b623adf5
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-09 10:15:17 +0900

    Remove legacy company code compatibility

--- app/models/company.rb
+++ app/models/company.rb
@@ -11,25 +11,6 @@ class Company < ApplicationRecord
   validates :domain, presence: true
   validates :domain, uniqueness: true
 
-  def self.upsert_all(attributes, **options)
-    normalized_attributes = attributes.map do |attribute|
-      next attribute unless attribute.key?(:code) && !attribute.key?(:domain)
-
-      attribute.except(:code).merge(domain: attribute[:code])
-    end
-    options[:unique_by] = :index_companies_on_domain if options[:unique_by].to_s == "index_companies_on_code"
-
-    super(normalized_attributes, **options)
-  end
-
-  def code
-    domain
-  end
-
-  def code=(value)
-    self.domain = value
-  end
-
   def display_name
     name.presence || domain
   end
