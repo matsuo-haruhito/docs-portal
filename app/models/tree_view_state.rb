0eccaef6bf8083a2dcee781c589cd2baff21b4e6
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-10 03:13:17 +0900

    Add tree view state model

---
+++ app/models/tree_view_state.rb
@@ -0,0 +1,7 @@
+class TreeViewState < ApplicationRecord
+  belongs_to :owner, polymorphic: true
+
+  validates :tree_instance_key, presence: true
+  validates :tree_instance_key, uniqueness: { scope: %i[owner_type owner_id] }
+  validates :expanded_keys, presence: true
+end
