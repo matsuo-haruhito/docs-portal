3aae10c967a5552e17f530efa9b4fe57f4a143dc
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-11 00:23:29 +0900

    Queue API specification build when source is stale

--- app/models/admin/api_specification_page.rb
+++ app/models/admin/api_specification_page.rb
@@ -1,7 +1,10 @@
+require "fileutils"
+require "open3"
+
 class Admin::ApiSpecificationPage
   SITE_PATH = "api-specification".freeze
 
-  def initialize(view_context:)
+  def initialize(view_context: nil)
     @view_context = view_context
   end
 
@@ -25,6 +28,49 @@ class Admin::ApiSpecificationPage
     build_entry_path.exist?
   end
 
+  def stale?
+    return true unless available?
+    return false unless source_path.exist?
+
+    source_path.mtime > build_entry_path.mtime
+  end
+
+  def build_requested?
+    build_request_marker_path.exist?
+  end
+
+  def request_build!
+    FileUtils.mkdir_p(build_request_marker_path.dirname)
+    File.write(build_request_marker_path, Time.current.iso8601)
+  end
+
+  def clear_build_request!
+    FileUtils.rm_f(build_request_marker_path)
+  end
+
+  def enqueue_build_if_stale!
+    return false unless stale?
+    return false if build_requested?
+
+    request_build!
+    ApiSpecificationBuildJob.perform_later
+    true
+  end
+
+  def build!
+    raise ActiveRecord::RecordNotFound, "API specification source is missing" unless source_path.exist?
+
+    FileUtils.mkdir_p(build_root)
+    stdout, stderr, status = Open3.capture3(
+      { "DOCUSAURUS_DOCS_PATH" => source_path.dirname.to_s },
+      "npm", "run", "build",
+      chdir: docusaurus_root.to_s
+    )
+    return if status.success? && build_entry_path.exist?
+
+    raise "API specification Docusaurus build failed: #{stderr.presence || stdout}"
+  end
+
   def iframe_src
     @view_context.site_admin_api_specification_path(site_path: SITE_PATH)
   end
@@ -40,8 +86,16 @@ class Admin::ApiSpecificationPage
 
   private
 
+  def docusaurus_root
+    Rails.root.join("docusaurus")
+  end
+
   def build_root
-    Rails.root.join("docusaurus", "build")
+    docusaurus_root.join("build")
+  end
+
+  def build_request_marker_path
+    Rails.root.join("tmp", "api_specification_build.requested")
   end
 
   def docusaurus_version
