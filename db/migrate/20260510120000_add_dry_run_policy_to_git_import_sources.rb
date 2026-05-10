ca0273eb17b2ccc1f2f40018f713d7a0807c541e
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-10 19:19:59 +0900

    Replace git dry run column with route settings

--- db/migrate/20260510120000_add_dry_run_policy_to_git_import_sources.rb
+++
@@ -1,5 +0,0 @@
-class AddDryRunPolicyToGitImportSources < ActiveRecord::Migration[8.1]
-  def change
-    add_column :git_import_sources, :dry_run_policy, :integer, null: false, default: 1
-  end
-end
