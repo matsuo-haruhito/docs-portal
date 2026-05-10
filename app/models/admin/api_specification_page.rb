ddf7c5b9775803865c608eeb7f451f9ac48a306b
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-10 23:44:36 +0900

    Add admin API specification page model

---
+++ app/models/admin/api_specification_page.rb
@@ -0,0 +1,72 @@
+class Admin::ApiSpecificationPage
+  SITE_PATH = "api-specification".freeze
+
+  def initialize(view_context:)
+    @view_context = view_context
+  end
+
+  def title
+    "API仕様"
+  end
+
+  def source_path
+    Rails.root.join("docs-src", "api-specification.md")
+  end
+
+  def build_entry_path
+    build_root.join(SITE_PATH, "index.html")
+  end
+
+  def available?
+    build_entry_path.exist?
+  end
+
+  def iframe_src
+    @view_context.admin_api_specification_site_path(site_path: SITE_PATH)
+  end
+
+  def render_site(site_path)
+    DocusaurusSiteRenderer.new(
+      version: docusaurus_version,
+      view_context: @view_context,
+      embedded: true,
+      site_url_builder: lambda { |relative_path, _version| @view_context.admin_api_specification_site_path(site_path: relative_path) }
+    ).render_html(site_path.presence || SITE_PATH)
+  end
+
+  private
+
+  def build_root
+    Rails.root.join("docusaurus", "build")
+  end
+
+  def docusaurus_version
+    Admin::ApiSpecificationPage::DocusaurusVersion.new(build_root:, entry_path: SITE_PATH)
+  end
+
+  DocusaurusVersion = Struct.new(:build_root, :entry_path, keyword_init: true) do
+    def site_root_absolute_path
+      build_root
+    end
+
+    def site_entry_relative_path
+      entry_path
+    end
+
+    def site_entry_absolute_path
+      build_root.join(entry_path, "index.html")
+    end
+
+    def legacy_html_absolute_path
+      build_root.join("index.html")
+    end
+
+    def site_build_path
+      entry_path
+    end
+
+    def html_view_site_path
+      entry_path
+    end
+  end
+end
