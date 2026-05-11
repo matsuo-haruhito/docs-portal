7b0c5a05bdffe9beef544ea736ef89bfdc666e01
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-12 03:02:44 +0900

    Fix Solid Cable messages schema

--- db/migrate/20260511180000_create_solid_cable_messages.rb
+++ db/migrate/20260511180000_create_solid_cable_messages.rb
@@ -4,10 +4,11 @@ class CreateSolidCableMessages < ActiveRecord::Migration[8.1]
       t.binary :channel, null: false, limit: 1024
       t.binary :payload, null: false, limit: 536870912
       t.datetime :created_at, null: false
-    end
+      t.integer :channel_hash, null: false, limit: 8
 
-    add_index :solid_cable_messages, :created_at
-    add_index :solid_cable_messages, :channel
-    add_index :solid_cable_messages, :channel, length: 1024, name: :index_solid_cable_messages_on_channel_hash
+      t.index :channel, name: :index_solid_cable_messages_on_channel
+      t.index :channel_hash, name: :index_solid_cable_messages_on_channel_hash
+      t.index :created_at, name: :index_solid_cable_messages_on_created_at
+    end
   end
 end
