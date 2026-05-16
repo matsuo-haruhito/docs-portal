eea0cd9d2693088830b982191343310ab6affa7f
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-17 07:49:20 +0900

    feat: render pdf document file preview

--- app/controllers/document_files_controller.rb
+++ app/controllers/document_files_controller.rb
@@ -35,6 +35,13 @@ class DocumentFilesController < BaseController
 
     record_file_access_log(file)
 
+    if disposition == "inline" && viewer_plan.viewer_kind == :pdf && !embedded_request?
+      response.headers["Content-Disposition"] = DocumentFileContentDisposition.new(file, disposition:).header
+      assign_preview_context(file)
+      render :show_pdf_preview
+      return
+    end
+
     if disposition == "inline" && viewer_plan.viewer_kind == :image && !embedded_request?
       response.headers["Content-Disposition"] = DocumentFileContentDisposition.new(file, disposition:).header
       assign_preview_context(file)
