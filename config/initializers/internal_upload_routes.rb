0bfca156e47a52c6f5f44520cbd25587327b49d0
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-19 12:14:03 +0900

    fix: route file upload API to manual uploads controller

--- config/initializers/internal_upload_routes.rb
+++ config/initializers/internal_upload_routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.append do
   post "/api/internal/artifact_imports", to: "api/internal/artifact_imports#create"
-  post "/api/internal/file_uploads", to: "api/internal/file_uploads#create"
+  post "/api/internal/file_uploads", to: "api/internal/manual_uploads#create"
   post "/api/internal/zip_uploads", to: "api/internal/zip_uploads#create"
 end
