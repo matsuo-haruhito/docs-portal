05a768dbf7811af1860f7989d7f4b1821072023e
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-19 22:27:06 +0900

    feat: add file change event registry abstraction

---
+++ app/services/generated_files/file_change_event_registry.rb
@@ -0,0 +1,34 @@
+require "pathname"
+require "yaml"
+
+module GeneratedFiles
+  class FileChangeEventRegistry
+    DEFAULT_REGISTRY_PATH = "config/file_change_event_jobs.yml"
+
+    def initialize(registry_path: DEFAULT_REGISTRY_PATH, root: nil)
+      @root = Pathname(root || default_root).expand_path
+      @registry_path = absolute_path(registry_path)
+    end
+
+    def rules
+      @rules ||= YAML.safe_load(registry_path.read, permitted_classes: [], aliases: false).fetch("rules")
+    end
+
+    private
+
+    attr_reader :root, :registry_path
+
+    def default_root
+      if defined?(Rails)
+        Rails.root
+      else
+        Pathname(__dir__).join("..", "..", "..").expand_path
+      end
+    end
+
+    def absolute_path(path)
+      path = Pathname(path)
+      path.absolute? ? path : root.join(path)
+    end
+  end
+end
