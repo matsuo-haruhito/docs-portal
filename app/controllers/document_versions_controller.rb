6af5bdd9df217abb45c0eed583777c234fc17a9f
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-20 01:41:44 +0900

    feat: expose Docusaurus manifest on version page

--- app/controllers/document_versions_controller.rb
+++ app/controllers/document_versions_controller.rb
@@ -15,6 +15,7 @@ class DocumentVersionsController < BaseController
     @compare_version_options = compare_version_options
     @version_file_diff_summary = build_file_diff_summary(@version, @compare_version)
     @preview_target_display_summary = DocumentVersionPreviewTargetDisplaySummary.new(@version).call
+    @docusaurus_build_manifest = DocusaurusBuildManifest.new(@version).call
     @markdown_line_diffs = MarkdownLineDiffBuilder.new(
       current_version: @version,
       previous_version: @compare_version,
