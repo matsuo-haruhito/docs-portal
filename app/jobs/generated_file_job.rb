ad3e0945d2b4ec8fa7a2286732053bd0828ca5c4
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-19 10:12:48 +0900

    feat: carry generated file event metadata into job

--- app/jobs/generated_file_job.rb
+++ app/jobs/generated_file_job.rb
@@ -1,7 +1,13 @@
 class GeneratedFileJob < ApplicationJob
   queue_as :default
 
-  def perform(changed_files: [], job_ids: [])
+  def perform(changed_files: [], job_ids: [], event_source: nil, metadata: {})
+    Rails.logger.info(
+      "GeneratedFileJob started: event_source=#{event_source.inspect} " \
+      "job_ids=#{Array(job_ids).join(',')} changed_files=#{Array(changed_files).join(',')} " \
+      "metadata=#{metadata.to_h.inspect}"
+    )
+
     GeneratedFiles::Runner.new(changed_files:, job_ids:).call
   end
 end
