e1ca8f8a602e5e7e67fd3dd981802c7d64f02be7
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-20 02:34:29 +0900

    test: cover change event handler blank paths

--- spec/services/generated_files/change_event_handler_spec.rb
+++ spec/services/generated_files/change_event_handler_spec.rb
@@ -126,6 +126,32 @@ RSpec.describe GeneratedFiles::ChangeEventHandler do
     )
   end
 
+  it "ignores blank file event paths" do
+    registry = write_registry(
+      rules: [
+        {
+          "id" => "blank_matcher",
+          "operations" => ["update"],
+          "path_patterns" => ["*"],
+          "job_class" => "GeneratedFileJob",
+          "params" => {"changed_files" => "$matched_files"}
+        }
+      ]
+    )
+    allow(GeneratedFileJob).to receive(:perform_later)
+
+    rule_ids = described_class.new(
+      file_events: [{"path" => "", "operation" => "update"}, nil],
+      changed_files: ["", "./"],
+      registry_path: registry,
+      root: @root,
+      output: StringIO.new
+    ).call
+
+    expect(rule_ids).to eq([])
+    expect(GeneratedFileJob).not_to have_received(:perform_later)
+  end
+
   it "treats changed_files as update events for backward compatibility" do
     registry = write_registry(
       rules: [
