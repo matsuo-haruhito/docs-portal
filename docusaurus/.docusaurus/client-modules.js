bf89ce85b44bc9dae35ba86686978c0b8349f012
matsuo-haruhito <matsuo@barnet.co.jp>
2026-05-20 23:06:08 +0900

    .docusaurusを追跡から除去

--- docusaurus/.docusaurus/client-modules.js
+++
@@ -1,5 +0,0 @@
-export default [
-  require("/app/docusaurus/node_modules/infima/dist/css/default/default.css"),
-  require("/app/docusaurus/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/app/docusaurus/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-];
