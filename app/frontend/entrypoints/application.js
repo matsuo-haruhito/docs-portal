69e7673a3985fac9041d501f25208327d7115cf2
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-25 17:13:45 +0900

    feat: add page bulk selection controls for document zips

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -5,6 +5,7 @@ import { Application } from "@hotwired/stimulus"
 import { RailsTablePreferencesController } from "rails_table_preferences"
 import { TomSelectController } from "rails_fields_kit"
 import "tom-select/dist/css/tom-select.css"
+import DocumentZipSelectionController from "../controllers/document_zip_selection_controller"
 import NavDropdownsController from "../controllers/nav_dropdowns_controller"
 import DocumentTreeNavigationController from "../controllers/document_tree_navigation_controller"
 import FileDropzoneController from "../controllers/file_dropzone_controller"
@@ -16,6 +17,7 @@ import SidebarController from "../controllers/sidebar_controller"
 const application = Application.start()
 application.register("rails-table-preferences", RailsTablePreferencesController)
 application.register("rails-fields-kit--tom-select", TomSelectController)
+application.register("document-zip-selection", DocumentZipSelectionController)
 application.register("nav-dropdowns", NavDropdownsController)
 application.register("document-tree-navigation", DocumentTreeNavigationController)
 application.register("file-dropzone", FileDropzoneController)
