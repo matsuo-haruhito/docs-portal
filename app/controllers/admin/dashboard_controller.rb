7d47a7a234a1b6abc4720c2d78117a68b1e62739
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-26 16:06:39 +0900

    fix: redirect company master admin from admin root

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,9 +1,18 @@
 class Admin::DashboardController < Admin::BaseController
-  before_action :require_admin_only!
+  before_action :redirect_company_master_admin_to_allowed_surface!, only: :index
+  before_action :require_admin_only!, only: :index
 
   def index
     @configuration_diagnostic = ApplicationConfigurationDiagnostic.new.call
     @document_file_health = DocumentFileHealthCheck.new.call
     @model_browser_entries = Admin::ModelBrowserCatalog.entries.first(8)
+  end
+
+  private
+
+  def redirect_company_master_admin_to_allowed_surface!
+    return unless company_master_admin?
+
+    redirect_to admin_companies_path
   end
 end
