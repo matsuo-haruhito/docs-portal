99eaf295a0e55d54d5269cb489391d7c0c16d1e5
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-27 13:13:30 +0900

    Reuse shared document filtering for document index

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -2,7 +2,6 @@ class DocumentsController < BaseController
   before_action :apply_rparam, only: :index
 
   DOCUMENTS_PER_PAGE = 20
-  DIAGRAM_EXTENSIONS = %w[puml plantuml d2 mmd mermaid].freeze
 
   def index
     @project = Project.find_by!(code: params[:project_code])
@@ -166,14 +165,7 @@ class DocumentsController < BaseController
   end
 
   def filtered_documents
-    scope = @project.documents.accessible_to(current_user)
-    scope = apply_keyword_filter(scope)
-    scope = apply_tag_filter(scope)
-    scope = apply_enum_filter(scope, :category, Document.categories)
-    scope = apply_enum_filter(scope, :document_kind, Document.document_kinds)
-    scope = apply_enum_filter(scope, :visibility_policy, Document.visibility_policies)
-    scope = apply_availability_filters(scope)
-    scope.distinct
+    ProjectDocumentFilter.new(project: @project, user: current_user, filters: @filters).call
   end
 
   def portal_tree_projects(include_project: nil)
@@ -227,68 +219,5 @@ class DocumentsController < BaseController
       return unless version&.document&.project_id == @project.id
       return unless version.viewable_by?(current_user)
     end
-  end
-
-  def apply_keyword_filter(scope)
-    DocumentSearch.new(@filters[:q]).apply(scope)
-  end
-
-  def apply_tag_filter(scope)
-    tag = @filters[:tag].to_s.strip
-    return scope if tag.blank?
-
-    scope
-      .joins(:document_tags)
-      .where(document_tags: { normalized_name: DocumentTag.normalize(tag) })
-  end
-
-  def apply_enum_filter(scope, key, enum_values)
-    value = @filters[key].to_s
-    return scope if value.blank? || !enum_values.key?(value)
-
-    scope.where(key => value)
-  end
-
-  def apply_availability_filters(scope)
-    scope = filter_html_available(scope) if enabled_filter?(:has_html)
-    scope = filter_file_attached(scope) if enabled_filter?(:has_files)
-    scope = filter_pdf_available(scope) if enabled_filter?(:has_pdf)
-    scope = filter_diagram_available(scope) if enabled_filter?(:has_diagram)
-    scope
-  end
-
-  def filter_html_available(scope)
-    html_version_ids = DocumentVersion.where.not(site_build_path: [nil, ""]).select(:id)
-    scope.where(latest_version_id: html_version_ids)
-  end
-
-  def filter_file_attached(scope)
-    document_ids = DocumentVersion.joins(:document_files).select(:document_id)
-    scope.where(id: document_ids)
-  end
-
-  def filter_pdf_available(scope)
-    scope
-      .left_joins(document_versions: :document_files)
-      .where(
-        "documents.document_kind = :pdf_kind OR LOWER(document_files.file_name) LIKE :pdf_file_name",
-        pdf_kind: Document.document_kinds[:pdf],
-        pdf_file_name: "%.pdf"
-      )
-  end
-
-  def filter_diagram_available(scope)
-    file_name_sql = DIAGRAM_EXTENSIONS.map { "LOWER(document_files.file_name) LIKE ?" }.join(" OR ")
-    file_name_patterns = DIAGRAM_EXTENSIONS.map { |extension| "%.#{extension}" }
-
-    scope
-      .left_joins(document_versions: :document_files)
-      .where(
-        ["document_versions.source_extension IN (?) OR #{file_name_sql}", DIAGRAM_EXTENSIONS, *file_name_patterns]
-      )
-  end
-
-  def enabled_filter?(key)
-    @filters[key] == true
   end
 end
