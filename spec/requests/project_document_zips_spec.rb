17adfdc624da23157a2318a5ae8d5731a6864c2b
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-27 13:20:05 +0900

    Expect matching selection button to count only selectable documents

--- spec/requests/project_document_zips_spec.rb
+++ spec/requests/project_document_zips_spec.rb
@@ -140,7 +140,7 @@ RSpec.describe "Project document zips", type: :request do
     expect(response.body).to include("選択した文書の最新版をZIPでダウンロード")
     expect(response.body).to include("ZIP出力オプション")
     expect(response.body).to include("このページを全選択")
-    expect(response.body).to include("検索結果2件を全選択")
+    expect(response.body).to include("検索結果1件を全選択")
     expect(response.body).to include("選択解除")
     expect(response.body).to include("0件選択中")
 
