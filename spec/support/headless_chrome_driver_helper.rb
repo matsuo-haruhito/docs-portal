68771437ad0b19d5303aa01871e18e153284afa6
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-31 03:20:46 +0900

    test: pass Chrome arguments through driven_by block

--- spec/support/headless_chrome_driver_helper.rb
+++ spec/support/headless_chrome_driver_helper.rb
@@ -1,5 +1,3 @@
-require "selenium/webdriver"
-
 module HeadlessChromeDriverHelper
   CHROME_ARGUMENTS = %w[
     --no-sandbox
@@ -8,10 +6,9 @@ module HeadlessChromeDriverHelper
   ].freeze
 
   def driven_by_headless_chrome(screen_size: [1400, 1400])
-    options = Selenium::WebDriver::Chrome::Options.new
-    CHROME_ARGUMENTS.each { |argument| options.add_argument(argument) }
-
-    driven_by(:selenium, using: :headless_chrome, screen_size:, options:)
+    driven_by(:selenium, using: :headless_chrome, screen_size:) do |options|
+      CHROME_ARGUMENTS.each { |argument| options.add_argument(argument) }
+    end
   end
 end
 
