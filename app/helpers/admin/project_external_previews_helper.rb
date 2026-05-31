647de4bb61dc1412caf85ffffdc5aa54555e0d9d
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-31 23:08:33 +0900

    design: 外部表示プレビューhelperをuser labelに限定

--- app/helpers/admin/project_external_previews_helper.rb
+++ app/helpers/admin/project_external_previews_helper.rb
@@ -5,19 +5,11 @@ module Admin::ProjectExternalPreviewsHelper
     users.map { |user| [external_preview_user_label(user), user.id] }
   end
 
-  def external_preview_company_options(companies)
-    companies.map { |company| [external_preview_company_label(company), company.id] }
-  end
-
   def external_preview_user_label(user)
     [
       user.display_name,
       user.email_address,
       user.company&.display_name
     ].compact_blank.join(" / ")
-  end
-
-  def external_preview_company_label(company)
-    [company.display_name, company.domain].compact_blank.join(" / ")
   end
 end
