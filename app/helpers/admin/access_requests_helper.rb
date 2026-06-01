f33d449df3fc3c67eb599619280bc2a38ea63c7b
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-06-01 21:07:15 +0900

    Add access request filter labels

--- app/helpers/admin/access_requests_helper.rb
+++ app/helpers/admin/access_requests_helper.rb
@@ -5,11 +5,33 @@ module Admin::AccessRequestsHelper
     "rejected" => "却下",
     "cancelled" => "取消済み"
   }.freeze
+  ACCESS_REQUEST_ACCESS_LEVEL_LABELS = {
+    "view" => "閲覧",
+    "download" => "ダウンロード",
+    "manage" => "管理"
+  }.freeze
+  ACCESS_REQUEST_REQUESTABLE_TYPE_LABELS = {
+    "Project" => "案件",
+    "Document" => "文書",
+    "DocumentFile" => "文書ファイル"
+  }.freeze
   DEFAULT_ACCESS_REQUEST_REJECTION_REASON = "承認条件を満たしていないため却下しました".freeze
 
   def admin_access_request_status_filter_options
     [["すべて", nil]] + AccessRequest.statuses.keys.map do |status|
       [admin_access_request_status_label(status), status]
+    end
+  end
+
+  def admin_access_request_access_level_filter_options
+    [["すべて", nil]] + AccessRequest.requested_access_levels.keys.map do |access_level|
+      [admin_access_request_access_level_label(access_level), access_level]
+    end
+  end
+
+  def admin_access_request_requestable_type_filter_options
+    [["すべて", nil]] + AccessRequest::SUPPORTED_REQUESTABLE_TYPES.map do |requestable_type|
+      [admin_access_request_requestable_type_label(requestable_type), requestable_type]
     end
   end
 
@@ -22,6 +44,26 @@ module Admin::AccessRequestsHelper
     return "すべて" if status.blank?
 
     admin_access_request_status_label(status)
+  end
+
+  def admin_access_request_access_level_label(access_level)
+    ACCESS_REQUEST_ACCESS_LEVEL_LABELS.fetch(access_level.to_s, access_level.to_s)
+  end
+
+  def admin_access_request_access_level_filter_label(access_level)
+    return "すべて" if access_level.blank?
+
+    admin_access_request_access_level_label(access_level)
+  end
+
+  def admin_access_request_requestable_type_label(requestable_type)
+    ACCESS_REQUEST_REQUESTABLE_TYPE_LABELS.fetch(requestable_type.to_s, requestable_type.to_s)
+  end
+
+  def admin_access_request_requestable_type_filter_label(requestable_type)
+    return "すべて" if requestable_type.blank?
+
+    admin_access_request_requestable_type_label(requestable_type)
   end
 
   def admin_access_request_primary_target_label(requestable)
