6fce412b109cc358ebb46bb95d2f2a1f51ce01ba
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-06-02 23:15:31 +0900

    Add document set document search route

--- config/routes.rb
+++ config/routes.rb
@@ -78,7 +78,9 @@ Rails.application.routes.draw do
       patch :restore, on: :member
     end
     resources :bulk_edit_dry_runs, only: %i[new create show update], param: :public_id
-    resources :document_sets, except: %i[show new], param: :public_id
+    resources :document_sets, except: %i[show new], param: :public_id do
+      get :document_search, on: :collection
+    end
     resources :document_permissions, except: %i[show new], param: :public_id
     resources :webhook_endpoints, except: %i[show new], param: :public_id
     resources :webhook_deliveries, only: [:show], param: :public_id do
