e90e052930a267650068153405d0ca5fff220e19
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-06-10 20:19:11 +0900

    chore: add dedicated preview helper controllers

---
+++ app/frontend/controllers/site_viewer_iframe_height_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus"
+import { setupSiteViewerIframeHeightSync } from "../lib/site_viewer_iframe_height"
+
+export default class extends Controller {
+  connect() {
+    this.refresh = this.refresh.bind(this)
+    document.addEventListener("turbo:load", this.refresh)
+    document.addEventListener("turbo:render", this.refresh)
+    this.refresh()
+  }
+
+  disconnect() {
+    document.removeEventListener("turbo:load", this.refresh)
+    document.removeEventListener("turbo:render", this.refresh)
+  }
+
+  refresh() {
+    setupSiteViewerIframeHeightSync()
+  }
+}
