22550fca8ca7f17b3e37207ce35faeadd97a34a7
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-06-11 12:06:10 +0900

    design: add Microsoft Graph table preference columns

--- app/helpers/admin/microsoft_graph_connections_helper.rb
+++ app/helpers/admin/microsoft_graph_connections_helper.rb
@@ -9,6 +9,19 @@ module Admin::MicrosoftGraphConnectionsHelper
   GRAPH_SENSITIVE_VALUE_PATTERN = /(\b(?:authorization|token|secret|password|client_secret|access_token|refresh_token)\b\s*[:=]\s*)([^\s&;,]+)/i.freeze
   GRAPH_SENSITIVE_QUERY_PATTERN = /([?&](?:token|secret|password|client_secret|access_token|refresh_token)=)([^&\s]+)/i.freeze
 
+  def microsoft_graph_connection_table_columns
+    [
+      table_preferences_column(:project, label: "案件", default_width: 220, pinned: true, overflow: :ellipsis),
+      table_preferences_column(:name, label: "接続名", default_width: 200, overflow: :ellipsis, sortable: true),
+      table_preferences_column(:graph_identifiers, label: "Tenant / Client / Site", default_width: 280, overflow: :ellipsis),
+      table_preferences_column(:drive, label: "Drive", default_width: 260, overflow: :ellipsis),
+      table_preferences_column(:preview_folder, label: "プレビュー用フォルダ", default_width: 260, overflow: :ellipsis),
+      table_preferences_column(:status, label: "状態", default_width: 110),
+      table_preferences_column(:preview_usage, label: "preview利用", default_width: 220),
+      table_preferences_column(:actions, label: "操作", default_width: 230, pinned: true)
+    ]
+  end
+
   def microsoft_graph_connection_identifier_preview(value)
     preview = microsoft_graph_connection_sanitized_identifier(value)
     return "-" if preview.blank?
