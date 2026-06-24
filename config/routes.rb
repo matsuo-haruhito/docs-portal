6e160a089503be1657a569c6637268bda7753772
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-06-25 01:13:08 +0900

    fix: preserve project consent selected project route

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,8 @@ Rails.application.routes.draw do
     resources :consent_terms, except: %i[show new], param: :public_id
     resources :project_consent_settings, except: %i[show new], param: :public_id do
       get :project_search, on: :collection
+      get :selected_project, on: :collection
+      get :consent_term_search, on: :collection
       get :selected_consent_term, on: :collection
     end
     resources :git_import_sources, except: %i[show new], param: :public_id do
