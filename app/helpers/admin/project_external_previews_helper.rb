102971a1fbc463972b81e81b708bdd5bb910e038
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-06-28 17:09:45 +0900

    feat: add external preview selected option helpers

--- app/helpers/admin/project_external_previews_helper.rb
+++ app/helpers/admin/project_external_previews_helper.rb
@@ -5,11 +5,29 @@ module Admin::ProjectExternalPreviewsHelper
     users.map { |user| [external_preview_user_label(user), user.id] }
   end
 
+  def external_preview_user_selected_option(user)
+    return if user.blank?
+
+    { value: user.id, text: external_preview_user_label(user) }
+  end
+
   def external_preview_user_label(user)
     [
       user.display_name,
       user.email_address,
       user.company&.display_name
     ].compact_blank.join(" / ")
+  end
+
+  def external_preview_company_selected_option(company)
+    return if company.blank?
+
+    { value: company.id, text: external_preview_company_label(company) }
+  end
+
+  def external_preview_company_label(company)
+    label = company.display_name
+    label = "#{label} / #{company.domain}" if company.domain.present?
+    label
   end
 end
