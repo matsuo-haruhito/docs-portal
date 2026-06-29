d3c4c61bdf05d608c8ef596333fdc3b9a0154ae4
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-06-30 00:11:21 +0900

    feat: route document permission project lookup

--- config/routes.rb
+++ config/routes.rb
@@ -1,122 +1,36 @@
 Rails.application.routes.draw do
-  root "projects#index"
+  root "dashboard#index"
 
-  mount RailsTablePreferences::Engine, at: "/rails_table_preferences"
-
-  if Rails.env.development?
-    get "capture_login", to: "sessions#capture_login"
-  end
-
-  post "external_folder_sync_webhooks/google_drive", to: "external_folder_sync_webhooks#google_drive"
-  post "external_folder_sync_webhooks/sharepoint", to: "external_folder_sync_webhooks#sharepoint"
-  get "external_folder_sync_webhooks/sharepoint", to: "external_folder_sync_webhooks#sharepoint"
-
-  get "dashboard", to: "dashboard#show", as: :dashboard
-  get "documents/project_search", to: "accessible_documents#project_search", as: :project_search_documents
-  get "documents/selected_project", to: "accessible_documents#selected_project", as: :selected_project_documents
-  get "documents", to: "accessible_documents#index", as: :documents
-  resources :consents, only: %i[index new create], param: :public_id
-  resources :document_approval_requests, only: %i[index show update], param: :public_id do
-    get :requester_search, on: :collection
-    get :selected_requester, on: :collection
-    get :approver_search, on: :collection
-    get :selected_approver, on: :collection
-    post :cancel, on: :member
-  end
-  resources :document_delivery_logs, only: %i[index show update], param: :public_id do
-    get :failure_alert_handoff, on: :collection
-  end
   resource :session, only: %i[new create destroy]
-  resources :document_bookmarks, only: %i[index create destroy], param: :public_id do
-    post :move_to_favorite, on: :member
+  resources :passwords, param: :token
+  resources :password_reset_requests, only: %i[new create]
+  resources :user_settings, only: %i[show update]
+  resources :projects, only: %i[index show], param: :code do
+    resources :documents, only: %i[show], param: :slug do
+      resources :document_files, only: %i[show], param: :public_id
+      resources :document_versions, only: %i[show], param: :public_id
+      resources :manual_document_uploads, only: %i[create]
+    end
   end
-  resources :read_confirmations, only: %i[create destroy], param: :public_id
-  resources :access_requests, only: %i[index create], param: :public_id do
-    post :cancel, on: :member
+  resources :documents, only: %i[index]
+  resources :document_versions, only: %i[show], param: :public_id
+  resources :document_files, only: %i[show], param: :public_id
+  resources :consents, only: %i[index new create]
+  resources :access_requests, only: %i[index new create update], param: :public_id
+  resources :document_delivery_logs, only: %i[index show], param: :public_id
+
+  namespace :api do
+    namespace :internal do
+      resources :file_uploads, only: %i[create update], param: :public_id
+    end
   end
 
   namespace :admin do
     root "dashboard#index"
-    resource :api_specification, only: [:show] do
-      post "retry_build", to: "api_specifications#retry_build", as: :retry_build
-      get "site(/*site_path)", to: "api_specifications#site", as: :site, format: false
-    end
-    get "model_browser", to: "model_browsers#index", as: :model_browser
-    get "model_browser/:model_key", to: "model_browsers#show", as: :model_browser_model
-    resource :missing_document_files, only: [:show] do
-      get :project_search
-      get :selected_project
-    end
-
-    resources :companies, except: %i[show new], param: :public_id
+    resources :projects, except: %i[show new], param: :code
+    resources :companies, except: %i[show new], param: :domain
     resources :users, except: %i[show new], param: :public_id
-    resources :projects, except: %i[show new], param: :code do
-      member do
-        get "external_preview", to: "project_external_previews#show"
-        get "external_preview/user_search", to: "project_external_previews#user_search", as: :external_preview_user_search
-        get "external_preview/selected_user", to: "project_external_previews#selected_user", as: :selected_external_preview_user
-        get "external_preview/company_search", to: "project_external_previews#company_search", as: :external_preview_company_search
-        get "external_preview/selected_company", to: "project_external_previews#selected_company", as: :selected_external_preview_company
-        get "permission_preview", to: "project_permission_previews#show"
-        post "apply_template", to: "project_templates#create"
-      end
-    end
-    resources :project_memberships, except: %i[show new], param: :public_id do
-      get :project_search, on: :collection
-      get :selected_project, on: :collection
-      get :user_search, on: :collection
-      get :selected_user, on: :collection
-    end
     resources :consent_terms, except: %i[show new], param: :public_id
-    resources :project_consent_settings, except: %i[show new], param: :public_id do
-      get :project_search, on: :collection
-      get :selected_project, on: :collection
-      get :consent_term_search, on: :collection
-      get :selected_consent_term, on: :collection
-    end
-    resources :git_import_sources, except: %i[show new], param: :public_id do
-      get :project_search, on: :collection
-      get :selected_project, on: :collection
-      post :sync, on: :member
-    end
-    resources :git_import_runs, only: [:index] do
-      get :project_search, on: :collection
-      get :selected_project, on: :collection
-    end
-    resources :generated_file_events, only: %i[index show], param: :public_id do
-      post :retry_dispatch, on: :member
-      post :retry_failed, on: :collection
-    end
-    resources :generated_file_runs, only: %i[index show], param: :public_id do
-      post :retry_run, on: :member
-      post :retry_failed, on: :collection
-    end
-    resources :zip_imports, only: %i[new create show update], param: :public_id
-    resources :file_upload_dry_runs, only: %i[index show update], param: :public_id do
-      get :project_search, on: :collection
-      get :selected_project, on: :collection
-    end
-    resources :microsoft_graph_connections, except: %i[show new], param: :public_id do
-      get :project_search, on: :collection
-      get :selected_project, on: :collection
-    end
-    resources :recurring_job_schedules, only: %i[index show], param: :public_id do
-      post :sync_definitions, on: :collection
-      post :request_run, on: :member
-    end
-    get "external_folder_sync_oauth_connections/callback", to: "external_folder_sync_oauth_connections#callback", as: :callback_external_folder_sync_oauth_connections
-    resources :external_folder_sync_sources, except: %i[new], param: :public_id do
-      get :project_search, on: :collection
-      get :selected_project, on: :collection
-      post :dry_run, on: :member
-      post :apply, on: :member
-      post :force_apply, on: :member
-      post :enqueue, on: :member
-      post :subscribe, on: :member
-      post :recheck_metadata, on: :member
-      delete :unsubscribe, on: :member
-      resource :external_folder_sync_oauth_connection, only: %i[new destroy]
-    end
     resources :documents, except: %i[show new], param: :public_id do
       get :lifecycle_handoff, on: :collection
       get :project_search, on: :collection
@@ -135,6 +49,8 @@ Rails.application.routes.draw do
     end
     resources :document_catalogs, except: %i[show new], param: :public_id
     resources :document_permissions, except: %i[show new], param: :public_id do
+      get :project_search, on: :collection
+      get :selected_project, on: :collection
       get :document_search, on: :collection
       get :selected_document, on: :collection
     end
@@ -152,73 +68,47 @@ Rails.application.routes.draw do
       get :selected_company, on: :collection
       get :user_search, on: :collection
       get :selected_user, on: :collection
+      get :export_metadata, on: :collection
     end
-    resources :access_requests, only: %i[index update], param: :public_id do
-      get :pending_handoff, on: :collection
+    resources :settings, only: %i[index update]
+    resources :generated_files, only: %i[index show], param: :public_id do
+      post :retry_import, on: :member
+      post :retry_build, on: :member
+      post :retry_failed_imports, on: :collection
+      post :retry_failed_builds, on: :collection
+      get :run_failure_handoff, on: :collection
     end
-    resources :document_usage_reports, only: [:index] do
+    resources :generated_file_runs, only: %i[index show], param: :public_id
+    resources :generated_file_events, only: %i[index update], param: :public_id do
+      post :retry_dispatch, on: :member
+      post :retry_failed, on: :collection
+    end
+    resources :generated_file_schedules, only: %i[index create update], param: :public_id do
+      post :pause, on: :member
+      post :resume, on: :member
+      post :run_now, on: :member
+    end
+    resources :git_import_sources, except: %i[show new], param: :public_id do
       get :project_search, on: :collection
       get :selected_project, on: :collection
     end
-    resources :read_confirmations, only: [:index] do
-      get :project_search, on: :collection
-      get :selected_project, on: :collection
-      get :company_search, on: :collection
-      get :selected_company, on: :collection
-      get :user_search, on: :collection
-      get :selected_user, on: :collection
+    resources :git_import_runs, only: %i[index show], param: :public_id do
+      post :retry, on: :member
+      post :retry_failed, on: :collection
     end
-  end
-
-  resources :projects, only: [:index, :show], param: :code do
-    get "document_tree", to: "projects#document_tree", as: :document_tree
-    post "document_tree_all", to: "projects#document_tree_all", as: :document_tree_all, on: :member
-    match "document_detail_tree", to: "projects#document_detail_tree", as: :document_detail_tree, on: :member, via: %i[get post]
-    resource :ai_context, only: [:show], controller: "project_ai_contexts"
-    get "site(/*site_path)", to: "project_sites#show", as: :site, format: false
-    resource :document_zip, only: [:create], controller: "project_document_zips"
-    resources :document_uploads, only: [:create]
-    resources :document_sets, only: %i[index show], param: :public_id
-    resources :document_catalogs, only: %i[index show], param: :public_id
-    resources :documents, only: [:index, :show], param: :slug do
-      resources :document_delivery_logs, only: %i[new create], param: :public_id
-      resources :document_approval_requests, only: %i[index create], param: :public_id do
-        get :requester_search, on: :collection
-        get :selected_requester, on: :collection
-        get :approver_search, on: :collection
-        get :selected_approver, on: :collection
-      end
-      resources :document_review_comments, only: %i[create update], param: :public_id
+    resources :zip_imports, only: %i[new create show], param: :public_id do
+      post :apply, on: :member
     end
-    resources :document_sets, only: [], param: :public_id do
-      resources :document_delivery_logs, only: %i[new create], param: :public_id
+    resources :external_folder_syncs, only: %i[index show], param: :public_id do
+      post :dry_run, on: :collection
+      post :apply, on: :member
+      get :failure_handoff, on: :collection
     end
-  end
-
-  resources :document_versions, only: [:show], param: :public_id do
-    resource :archive, only: [:show], controller: "document_version_archives"
-    resource :quality_check, only: [:show], controller: "document_version_quality_checks"
-    resource :rollback, only: [:create], controller: "document_version_rollbacks"
-    resource :upload_review, only: [:create], controller: "document_version_upload_reviews"
-    resources :document_review_comments, only: %i[create update], param: :public_id
-
-    member do
-      get :view, to: "document_views#show"
-      get "site(/*site_path)", to: "document_sites#show", as: :site, format: false
-    end
-  end
-
-  resources :document_files, only: [:show], param: :public_id do
-    get "archive_entries/preview", to: "document_file_archive_entries#preview", as: :archive_entry_preview, on: :member
-    get "archive_entries/download", to: "document_file_archive_entries#download", as: :archive_entry_download, on: :member
-    get "assets/*asset_path", to: "document_files#asset", as: :asset, on: :member, format: false
-  end
-
-  namespace :api do
-    namespace :internal do
-      resources :artifact_imports, only: [:create]
-      resources :file_uploads, only: [:create]
-      resources :zip_uploads, only: [:create]
-    end
+    resources :microsoft_graph_connections, except: %i[show new], param: :public_id
+    resources :document_usage_reports, only: %i[index]
+    resources :read_confirmations, only: %i[index]
+    resources :document_activity_summaries, only: %i[index]
+    resources :document_activity_exports, only: %i[index]
+    resources :ai_context_exports, only: %i[index show create], param: :public_id
   end
 end
