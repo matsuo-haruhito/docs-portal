429d1e088b09dcff06ed8dda12de94a8e5031d48
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-07-03 09:08:23 +0900

    feat: expose storage docs and import details

--- app/controllers/admin/storage_usage_controller.rb
+++ app/controllers/admin/storage_usage_controller.rb
@@ -4,4 +4,12 @@ class Admin::StorageUsageController < Admin::BaseController
   def document_files
     @document_file_storage_usage_detail = StorageUsageSummary.new.document_file_detail
   end
+
+  def docs_sites
+    @storage_area_detail = StorageUsageSummary.new.docs_site_detail
+  end
+
+  def imports
+    @storage_area_detail = StorageUsageSummary.new.import_detail
+  end
 end
