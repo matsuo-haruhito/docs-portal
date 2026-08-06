07f98b2b0e6570b59eeeb7359580eba2e14d5694
matsuo-haruhito <matsuo@barnet.co.jp>
2026-08-07 00:22:51 +0900

    アクセス申請の manage 要求を承認処理で安全に扱う first slice
    manage 要求を grant_access! で自動承認しようとした場合に BadRequest を
    raise して明示的に止める。管理者が直接権限設定する運用に閉じる。
    view / download の既存承認フローには影響なし。
    
    Closes #1126

--- app/services/access_request_resolver.rb
+++ app/services/access_request_resolver.rb
@@ -59,6 +59,10 @@ class AccessRequestResolver
   attr_reader :access_request, :approver
 
   def grant_access!
+    if access_request.manage?
+      raise ApplicationError::BadRequest, "manage 要求の自動承認は現在サポートされていません。管理者が権限を直接設定してください。"
+    end
+
     case access_request.requestable
     when Project
       ProjectMembership.find_or_create_by!(project: access_request.requestable, user: access_request.requester) do |membership|
